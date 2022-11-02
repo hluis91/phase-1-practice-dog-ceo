6979db558354c4539774a114a15a472f9a4b3286
Luis Hernandez <lshrnndz6@gmail.com>
2022-11-01 21:54:41 -0700

    done

--- src/index.js
+++ src/index.js
@@ -1,1 +1,81 @@
-console.log('%c HI', 'color: firebrick')
+//Global variables
+const container = document.querySelector("#dog-image-container")
+const imgUrl = "https://dog.ceo/api/breeds/image/random/4"
+const breedUrl = 'https://dog.ceo/api/breeds/list/all'
+const ulContainer = document.querySelector("#dog-breeds")
+const dropdown = document.querySelector("#breed-dropdown")
+let breedsArray;
+//Event Listeners
+ulContainer.addEventListener('click', handleClick)
+dropdown.addEventListener('change', handleChange)
+// //Challenge 1
+function getImages(){
+    fetch(imgUrl)
+    .then(resp => resp.json())
+    .then(images => {
+        const imgs = images.message
+        let imgsArray = createImgElement(imgs)
+        renderImgs(imgsArray) 
+    })
+}
+
+function createImgElement(imgs){
+    return imgs.map((img) => {
+        let i =`<img src=${img}>`
+        return i
+    })
+}
+
+function renderImgs(imgsArray){
+    imgsArray.forEach(element => {
+        renderElement(element)
+    })
+}
+
+function renderElement(element) {
+    ulContainer.innerHTML += element
+}
+
+
+//Challenge 2
+
+function getBreeds(){
+    fetch(breedUrl)
+    .then(resp => resp.json())
+    .then(breeds => {
+        breedsArray = Object.keys(breeds.message)
+        const breedsLis = createLiElement(breedsArray)
+        renderLis(breedsLis) 
+    })
+}
+
+function createLiElement(breedsArray){
+    return breedsArray.map((breed) => {
+        let li =`<li>${breed}</li>`
+        return li
+    })
+}
+
+function renderLis(breedsLis){
+    breedsLis.forEach(element => {
+        renderElement(element)
+    })
+}
+
+// challenge 3
+
+function handleClick(event){
+    event.target.style.color = 'blue'
+}
+
+//challenge 4
+function handleChange(event){
+    const letter = event.target.value
+    const filteredBreeds = breedsArray.filter(breed => breed.startsWith(letter))
+    const filteredBreedsLis = createLiElement(filteredBreeds)
+    ulContainer.innerHTML = ''
+    renderLis(filteredBreedsLis)
+}
+
+getImages()
+getBreeds()
